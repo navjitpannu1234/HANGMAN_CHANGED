ddff51b178041a46e2dd9944245ab13f4dab51a9
tanveer99 <tanveerpannu99@gmail.com>
2020-08-16 18:31:06 +1200

    Add project files.

---
+++ Hangman_4444/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace Hangman_4444
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public TextView txt_player_Name;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+            Button btn_save = FindViewById<Button>(Resource.Id.btn_save);
+            btn_save.Click += btn_save_Click;
+            txt_player_Name = FindViewById<EditText>(Resource.Id.txt_player_Name);
+
+
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            if (txt_player_Name.Text.Length > 0)
+            {
+                
+                var con = new ConnectionClass();
+                
+                con.InsertNewPlayer(txt_player_Name.Text  , 0);
+
+
+                Toast.MakeText(this, "Record Save", ToastLength.Short).Show();
+                StartActivity(typeof(Play_Game));
+
+            }
+          
+            else
+            {
+
+
+                Toast.MakeText(this, "Please Fill Name", ToastLength.Short).Show();
+            }
+
+        }
+    }
+}
---
+++ Hangman_4444/view_score.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Hangman_4444.Resources;
+using SQLite;
+
+namespace Hangman_4444
+{
+    [Activity(Label = "view_score")]
+    public class view_score : Activity
+    {
+        List<Playerscores > myList;
+        public ConnectionClass  myConnectionClass;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.view_score );
+
+            myConnectionClass = new ConnectionClass();
+            myList = myConnectionClass.ViewAll();
+
+            Button btn_Player = FindViewById<Button>
+                (Resource.Id.btn_Player);
+
+            btn_Player.Click += btn_Player_Click;
+
+            // Display the player names and high scores 
+            ListView Lv_HighScore = FindViewById<ListView>(Resource.Id.Lv_HighScore);
+            Lv_HighScore.Adapter = new DataAdpter (this, myList);
+        }
+
+        private void btn_Player_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
